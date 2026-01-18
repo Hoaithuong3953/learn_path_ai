50c609ae2e198ba524f6f9f5de4c84a66954d6f1
TrinhThuong <trinhthuong0203@gmail.com>
2026-01-18 15:43:26 +0700

    refactor(utils): remove unused RetryError import and use tuple for TRANSIENT_ERRORS

--- utils/retry.py
+++ utils/retry.py
@@ -1,14 +1,14 @@
-from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type, before_sleep_log, RetryError
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type, before_sleep_log
 from google.api_core import exceptions as google_exceptions
 
 from utils.logger import logger
 
-TRANSIENT_ERRORS = [
+TRANSIENT_ERRORS = (
     google_exceptions.DeadlineExceeded,
     google_exceptions.ResourceExhausted,
     google_exceptions.ServiceUnavailable,
     google_exceptions.Aborted
-]
+)
 
 def gemini_retry(max_retries: int = 3):
     return retry(
